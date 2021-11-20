b178c34af841fb3203ec06769877de41df9de72b
yeonju501 <loling970124@gmail.com>
2021-11-20 18:07:39 +0900

    update home

--- src/store/index.js
+++ src/store/index.js
@@ -1,22 +1,17 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import axios from 'axios'
+// import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
     isLogin: false,
-    
-    movieCards: [],
-    movieList: [],
-    genreList: [],
+
   },
 
   mutations: {
-    LOAD_MOVIE_CARDS: function(state, results) {
-      state.movieCards = results
-    },
+
   },
 
   actions: {
@@ -25,22 +20,6 @@ export default new Vuex.Store({
     //   context.commit('TOKEN', token);
     },
 
-    LoadMovieCards: function({commit}, token){
-      axios({
-        method: 'get',
-        url: 'http://127.0.0.1:8000/movies/',
-        headers: token
-        // params: {
-        //   api_key:  'de3308aff80fcad45743238dc9bd4d0d',
-        //   language: 'ko-KR',
-        // }
-      })
-        .then((res) => {
-          console.log(res)
-          commit('LOAD_MOVIE_CARDS', res.data)
-        })
-        .catch(err => console.log(err))
-    },
 
   modules: {
   }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -18,7 +18,8 @@
 // @ is an alias to /src
 import HomeMain from '@/components/HomeMain.vue'
 import MovieCard from '@/components/MovieCard.vue'
-import { mapState } from 'vuex'
+// import { mapState } from 'vuex'
+import axios from 'axios'
 // import jwt_decode from 'jwt-decode'
 
 export default {
@@ -27,21 +28,42 @@ export default {
     MovieCard,
     HomeMain,
   },
-
+  data: function() {
+    return {
+      movieCards: null,
+    }
+  },
   methods: {
-    SetToken: function () {
+    setToken: function () {
     const token = localStorage.getItem('jwt')
     const config = {
       Authorization: `JWT ${token}`
     }
     return config
   },
+    loadMovieCards: function(){
+      axios({
+        method: 'get',
+        url: 'http://127.0.0.1:8000/movies/',
+        headers: this.setToken(),
+
+      })
+        .then((res) => {
+          console.log(res)
+          this.movieCards = res.data
+        })
+        .catch(err => console.log(err))
+    },
+
   },
   created: function() {
-    this.$store.dispatch('LoadMovieCards')
+    if (localStorage.getItem('jwt')){
+      this.loadMovieCards()
+    } else {
+      this.$router.push({name:'Login'})
+    }
   },
   computed: {
-    ...mapState(['movieCards'])
   }
  }
 
